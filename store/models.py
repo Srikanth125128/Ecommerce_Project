9ab0f40014c1c51a1658c007e76c60e24c65410b
Srikanth125128 <srikanthmetpally70@gmail.com>
2025-03-19 18:52:29 +0530

    discounted_price

--- store/models.py
+++ store/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
